e33008360266271bd53598a2568169888c333a45
Adityaa-Sharma <mailmeifyoucan7@gmail.com>
2025-01-15 01:41:03 +0530

    addin the tokenization class

---
+++ Tokenization/__init__.py
@@ -0,0 +1,10 @@
+from .tokenization import CharacterTokenization, WordTokenization
+
+__version__ = '0.1.0'
+__author__ = 'Aditya Sharma'
+
+# Export classes for easier imports
+__all__ = [
+    'CharacterTokenization',
+    'WordTokenization',
+]
