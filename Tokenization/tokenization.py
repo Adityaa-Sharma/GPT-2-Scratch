3f44356282c97b474a679d8434ac7ffe869788e9
Adityaa-Sharma <mailmeifyoucan7@gmail.com>
2025-01-19 05:28:15 +0530

    few changes in word splitter

--- Tokenization/tokenization.py
+++ Tokenization/tokenization.py
@@ -1,35 +1,34 @@
 import re
 import tiktoken
-#character tokenizer
+
 class CharacterTokenization:
     def __init__(self, vocab: list):
         self.char_to_idx = {char: idx for idx, char in enumerate(vocab)}
         self.idx_to_char = {idx: char for idx, char in enumerate(vocab)}
     
-
     def encode(self, s: str) -> list:
-        encode=lambda s:[self.char_to_idx[char] for char in s]
-        return encode(s)
+        return [self.char_to_idx[char] for char in s]
 
     def decode(self, x: list) -> str:
-        decode=lambda x:''.join([self.idx_to_char[i] for i in x])
-        return decode(x)
-    
-    
+        return ''.join([self.idx_to_char[i] for i in x])
 
 class WordTokenization:
-    def __init__(self, vocab):  # Remove list type hint for now
+    def __init__(self, vocab):
+        # Add special tokens
+        vocab = ['<UNK>', '<PAD>'] + [word for word in vocab if word.strip()]
         self.word_to_idx = {word: idx for idx, word in enumerate(vocab)}
         self.idx_to_word = {idx: word for idx, word in enumerate(vocab)}
+        self.UNK_TOKEN = 0  # Index of <UNK>
 
-    def tokenize(self, s: str) -> list:
-        encode=lambda s:[self.word_to_idx[word] for word in s.split()]
-        return encode(s)
+    def encode(self, text: str) -> list:
+        if isinstance(text, list):
+            words = text
+        else:
+            words = text.split()
+        return [self.word_to_idx.get(word, self.UNK_TOKEN) for word in words]
 
-    def detokenize(self, tokens: list) -> str:
-        decode=lambda tokens: ' '.join([self.idx_to_word[i] for i in tokens])
-        return decode(tokens)
-
+    def decode(self, tokens: list) -> str:
+        return ' '.join([self.idx_to_word[i] for i in tokens])
 
 class GptTokenizer:
     def __init__(self):
@@ -40,8 +39,3 @@ class GptTokenizer:
     
     def decode(self, tokens: list) -> str:
         return self.tokenizer.decode(tokens)
-    
-
-        
-    
-#using the tiktoken library
--- utils/Splitter.py
+++ utils/Splitter.py
@@ -1,16 +1,19 @@
-
 import torch
+
 class DataSplitter:
-    def __init__(self, data, tokenizer, ratio):
+    def __init__(self, data, tokenizer, split_ratio):
         self.data = data
         self.tokenizer = tokenizer
-        self.ratio = ratio
-        
-
+        self.split_ratio = split_ratio
+    
     def split(self, ratio):
-        data=torch.tensor(self.tokenizer.encode(self.data),dtype=torch.long)
-        n=int(self.ratio*len(data))
-        train_data=data[:n]
-        val_data=data[n:]
-        return train_data, val_data
+        # Convert data to tensor using tokenizer
+        encoded = self.tokenizer.encode(self.data)
+        data = torch.tensor(encoded, dtype=torch.long)
         
+        # Split data
+        n = int(ratio * len(data))
+        train_data = data[:n]
+        val_data = data[n:]
+        return train_data, val_data
+
