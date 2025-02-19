8501f5800992394284bfcc4bbee3192eff9c8c40
Adityaa-Sharma <mailmeifyoucan7@gmail.com>
2025-02-20 01:15:14 +0530

    adding FastAttention Head

--- Model/MultiHeadAttention.py
+++ Model/MultiHeadAttention.py
@@ -10,6 +10,7 @@ class Head(nn.Module):
         self.key=nn.Linear(ModelConfig.n_embed,head_size,bias=False)
         self.query=nn.Linear(ModelConfig.n_embed,head_size,bias=False)
         self.value=nn.Linear(ModelConfig.n_embed,head_size,bias=False)
+        self.dropout = nn.Dropout(ModelConfig.dropout)
         self.register_buffer('tril',torch.tril(torch.ones(ModelConfig.block_size,ModelConfig.block_size))) # registering trill as it was not a parameter
         
     def forward(self,x):
@@ -21,17 +22,42 @@ class Head(nn.Module):
         wei=q@k.transpose(-2,-1)/C**0.5 # (B,T,C) @ (B,C,T) = (B,T,T)
         wei=wei.masked_fill(self.tril[:T,:T]==0,float('-inf'))
         wei=F.softmax(wei,dim=-1)
+        wei=self.dropout(wei)
         
         y=wei@v # (B,T,T) @ (B,T,C) = (B,T,C)
         return y
     
+
+
+class FastAttentionHead(nn.Module):
+    def __init__(self,head_size):
+        super().__init__()
+        self.key=nn.Linear(ModelConfig.n_embed,head_size,bias=False)
+        self.query=nn.Linear(ModelConfig.n_embed,head_size,bias=False)
+        self.value=nn.Linear(ModelConfig.n_embed,head_size,bias=False)
+        self.dropout = nn.Dropout(ModelConfig.dropout)
+        self.register_buffer('tril',torch.tril(torch.ones(ModelConfig.block_size,ModelConfig.block_size)))
+        
+    def forward(self,x):
+        B,T,C=x.shape
+        k=self.key(x)
+        q=self.query(x)
+        v=self.value(x)
+        
+        y=F.scaled_dot_product_attention(q,k,v,is_causal=True)
+        return y
     
+    
+
+
+
 class MultiHeadAttention(nn.Module):
     """ multiple heads of self-attention in parallel """
 
     def __init__(self, num_heads, head_size):
         super().__init__()
-        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        # self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.heads=nn.ModuleList([FastAttentionHead(head_size) for _ in range(num_heads)])
         self.proj = nn.Linear(ModelConfig.n_embed, ModelConfig.n_embed)
         self.dropout = nn.Dropout(ModelConfig.dropout)
 
