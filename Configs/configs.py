857d9674abca4f9e51f2a8d51541eb6e28b991db
Adityaa-Sharma <mailmeifyoucan7@gmail.com>
2025-02-26 22:27:26 +0530

    changing configs

--- Configs/configs.py
+++ Configs/configs.py
@@ -3,21 +3,22 @@ import torch
 
 @dataclass
 class ModelConfig:
-    
     # Training parameters
-    batch_size: int = 64
-    block_size: int = 256
-    max_iters: int = 5000
-    eval_interval: int = 500
-    eval_iter: int = 200
-    learning_rate: float = 6e-4 # gpt paper
-    n_epochs: int = 5
+    batch_size: int = 8 
+    block_size: int = 256 
+    max_iters: int = 480000
+    eval_interval: int = 5000  
+    eval_iter: int = 50
+    learning_rate: float = 3e-4  
+    n_epochs: int = 2
     
     # Model architecture
-    n_embed: int = 768
-    n_layer: int = 12 # gpt-2 124M model
-    n_head: int = 12 # gpt-2 124M model
+    n_embed: int = 384  
+    n_layer: int = 6   
+    n_head: int = 6    
     dropout: float = 0.1
     
-    # System
+    # Learning rate schedule
+    warmup_steps: int = 2000
+    lr_decay: bool = True
     device: torch.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
